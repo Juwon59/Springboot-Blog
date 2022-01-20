203df357251a21814d809b1d9e00767e4cc3774d
name <ajw5921@gmail.com>
2022-01-20 15:43:04 +0900

    function

--- src/main/resources/static/js/user.js
+++ src/main/resources/static/js/user.js
@@ -5,7 +5,16 @@ let index = {
         });
     },
     save:function () {
-        alert('user의 save함수 호출됨');
+        //alert('user의 save함수 호출됨');
+        let data = {
+            username: $("#username").val(),
+            password: $("#password").val(),
+            email: $("#email").val(),
+        }
+        //console.log(data);
+
+        $.ajax().done().fail(); //ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert 요청을 한다.
     }
 }
+
 index.init();
